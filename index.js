7b8a5dbbf8ea188e374985aa0f2fe8d5d6116dc1
Maks Bezrodnyi <maks.bezrodnyi@gmail.com>
2020-02-28 18:15:23 +0200

    line3

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ class HTMLel {
         this.spanLetter; 
         this.z = 1;
         this.fSize = "50px";
+
+        this.hulkText = "Bioscience genius Dr. Bruce Banner focused his major studies on the effects of gamma radiation on humans, working alongside Dr. Betty Ross at a Virginia University lab. Dr. Ross’ father, General “Thunderbolt” Ross of the U.S. Army, channeled Banner’s work into a program to develop weapons for the military, unbeknownst to both doctors. When Banner decided to test what he learned on himself, he triggered a fantastic transformation in himself that resulted in an immense, hulking figure fueled by his own rage. Having destroyed his lab as the creature, and inadvertently harming Ross in the process, Banner slipped away to avoid capture and study by the Army and began a desperate quest for a cure to his bizarre affliction."
+
+        this.ironText = "Genius inventor Tony Stark continued his father Howard Stark’s weaponry business after his parents’ untimely deaths and flew it to even greater heights of innovation. While in Afghanistan to demonstrate a new missile for the U.S. military, Stark’s convoy came under fire by a terrorist group known as the Ten Rings and he was severely wounded. Taken prisoner by the group, Stark awoke in their headquarters to learn that shrapnel near his heart had nearly cost him his life, but swift action by scientist and fellow prisoner Ho Yinsen—who had inserted a powerful electromagnet in Stark’s chest—would prolong it temporarily. Stalling his captors after they demanded he build them a new weapon, Stark replaced the magnet with the RT, a miniature version of a device originally designed by his father, the Arc Reactor. Furthermore, he and Yinsen created a crude suit of armor which could provide them the means with which to escape. The suit worked as planned, though Yinsen sacrificed himself in order to allow Tony enough time to power it up to fight their terrorist captors."       
+
         
         this.arrow.forEach(arrow => {
             arrow.addEventListener('click', this.slide.bind(this, arrow))
@@ -77,8 +82,27 @@ class HTMLel {
         let sub = el.nextElementSibling;
         el.classList.toggle('active');
         sub.classList.toggle('accordDisplay');
+        this.printed_el_text( sub );
         
     }
+
+    printed_el_text( el ){
+        let text;
+        if (el.classList.contains("hulk")) text = this.hulkText;
+        if (el.classList.contains("iron")) text = this.ironText;
+        let	i = 0;
+        var print = function print(){
+                i++;
+                if( i <= text.length ){
+                    el.innerHTML = text.substr(0, i);
+                    if(el.classList.contains('accordDisplay')) {
+                        setTimeout( print, 50 );
+                    }
+                    
+                }
+            };
+            print();
+    };
 
     toggleClass () {
          this.classList.toggle("change");        
@@ -114,4 +138,7 @@ class HTMLel {
 }
 
 let htmlStarter = new HTMLel();
+
+
+
 
